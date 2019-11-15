c5a72635e79751709cfb5725f37a8d9fc7f2bee6
Praveen Dasan <d.p.praveen@gmail.com>
2019-11-15 22:47:09 +0800

    chapter 07 completed loops

--- Chapter07.ps1
+++ Chapter07.ps1
@@ -42,3 +42,74 @@ $names | ForEach-Object {
 }
 
 #Section 7.5: Continue
+Write-Host "---------Continue--------"
+foreach ($i in 1..10) {
+    if ($i -eq 7) {
+        continue
+    }
+    Write-Host $i
+}
+
+Write-Host "------------While loop--------"
+$i = 0;
+while ($i -lt 7) {
+    $i++;
+    if ($i -eq 5) {
+        continue
+    }
+    Write-Host $i
+
+}
+
+Write-Host "--------------Break--------------"
+$i = 0;
+while ($i -lt 7) {
+    $i++;
+    if ($i -eq 3) {
+        break
+    }
+    Write-Host $i
+
+}
+
+Write-Host "----------------Break Labels-----------"
+$i = 0
+:mainloop while ($i -lt 15) {
+    Write-Host $i -ForegroundColor 'Cyan'
+    $j = 0
+    while ($j -lt 15) {
+        Write-Host $j -ForegroundColor 'Magenta'
+        $k = $i * $j
+        Write-Host $k -ForegroundColor 'Green'
+        if ($k -gt 100) {
+            break :mainloop
+        }
+        $j++
+    }
+    $i++
+}
+
+$i = 10
+while ($i -ge 0) {
+    $i
+    $i--
+}
+
+Write-Host "-----------While------------"
+
+Start-Process notepad.exe
+while (Get-Process notepad -ErrorAction SilentlyContinue) {
+    Start-Sleep -Milliseconds 500
+}
+
+
+$i = 0
+Do {
+    $i++
+    "Number $i"
+} while ($i -ne 3)
+$i = 0
+Do {
+    $i++
+    "Number $i"
+} until ($i -eq 3)
